2e3fe0995ca93814cbd7e2237c251ea4f7104f58
@NicChoo96 <nic_choo_96@hotmail.com>
2020-10-24 06:54:49 +0800

    base_algo_files_shortest_bfs

---
+++ graph_preprocessing.py
@@ -0,0 +1,65 @@
+import os
+from pyrgg import __main__ # need to install pyrgg first: pip install pyrgg==0.9 
+
+class Graph_Reader:
+    def __init__(self):
+        # self.graphFile = "roadNet-CA.txt"
+        self.graphFile = "as20000102.txt"
+        self.hospitalFile = "hospitalnodes.txt"
+
+
+    #takes in a textfile name as input, and returns a dictionary of node keys as output
+    #each node key has a value: the value is a list of adjacent nodes i.e. the nodes that can be reached from the node key
+    def readGraph(self):
+        graphFile = open(self.graphFile,"rb")
+        content = graphFile.read()
+
+        content_list = content.decode().split('\n')
+        graphFile.close()
+
+        graph = {}
+
+        content_list = content_list[:len(content_list)-1]
+
+        for line in content_list:
+            if '#' in line:
+                continue
+
+            linelist = line.split('\t')
+
+            if int(linelist[0]) in graph:
+                if int(linelist[1]) not in graph[int(linelist[0])]:
+                    graph[int(linelist[0])].append(int(linelist[1]))
+            else:
+                graph[int(linelist[0])] = [int(linelist[1])]
+
+            if int(linelist[1]) in graph:
+                if int(linelist[0]) not in graph[int(linelist[1])]:
+                    graph[int(linelist[1])].append(int(linelist[0]))
+            else:
+                graph[int(linelist[1])] = [int(linelist[0])]
+
+        return graph
+
+    #takes in a textfile name as input, and returns a list of hospital nodes as output
+    #textfile must follow the format stated (from project2 pdf):
+    # line 1:# numofhospitals   (note the hex and space after hex then your numofhospitals)
+    # line 2:node#   (repeat line 2 for each hospital node)
+    def readHospital(self):
+        hospitalFile = open(self.hospitalFile,"rb")
+        content = hospitalFile.read()
+        content_list = content.decode().split('\n')
+        hospitalFile.close()
+        k_hospital = int(content_list[0][2:])
+        hospital = []
+        for line in range(k_hospital):
+            hospital.append(int(content_list[line+1]))
+
+        return hospital
+
+    #input randomgraph as filename, 10 as graph format, 2 for unweighted, 2 for undirected, 1 for simple, any for self loop, 2 for unsigned, any edge and vertice number
+    #creates a tsv file to your current working directory (address printed as output), and returns the dictionary of node keys as well
+    def generateRandomGraph(self, readGraph):
+        __main__.run()
+        randomGraph  =  readGraph(os.getcwd()+"\\randomgraph.tsv")
+        return randomGraph
